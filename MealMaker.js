26c64fd791e8a960b327f0f7013f704537a593b2
DeNicholas <TP06291@mail.apu.edu.my>
2021-03-30 15:15:54 +0800

    Updated meal maker js

--- MealMaker.js
+++ MealMaker.js
@@ -11,9 +11,36 @@ const menu={
   get dessert (){},
   set dessert (des){},
   get _courses (){
-    return  Object.entries(appetizers), Object.entries(mains), Object.entries(dessert)
+    return  this.appetizers, this.mains, this.dessert
   },
-  const addDishToCourse=(courseName, dishName, dishPrice)=>{
-
+   addDishToCourse (courseName, dishName, dishPrice){
+    const dish = {
+      name: courseName,
+      price: dishPrice,
+    };
+   this._courses[courseName].push(dish);
+  },
+   getRandomDishFromCourse (courseName){
+    const dishes= menu._courses[courseName];
+    let num= Math.floor(Math.random()*dishes.length)
+    return dishes[num]
+  },
+  generateRandomMeal(){
+    const appetizer= this.getRandomDishFromCourse('appetizers')
+    const main= this.getRandomDishFromCourse('mains')
+    const dessert = this.getRandomDishFromCourse('dessert')
+    const totalPrice = appetizer.price + main.price + dessert.price
+    return `the price is RM${totalPrice} and the items that you ordered are ${appetizer}, ${main} and ${dessert}`
   }
 };
+menu.addDishToCourse('appetizers', 'kacang', "1")
+menu.addDishToCourse('appetizers', 'french fries', "5")
+menu.addDishToCourse('appetizers', 'some french stuff', "7")
+menu.addDishToCourse('main', 'steak', "35")
+menu.addDishToCourse('main', 'chee cheong fun', "10")
+menu.addDishToCourse('main', 'roti canai', "5")
+menu.addDishToCourse('dessert', 'ice cream', "6")
+menu.addDishToCourse('dessert', 'fruits', "15")
+menu.addDishToCourse('dessert', 'chocolate', "9")
+
+  const meal= menu.generateRandomMeal()
