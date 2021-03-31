2d8ee3f99aa14d2c825da57a30f99f5a5a89243a
DeNicholas <TP06291@mail.apu.edu.my>
2021-03-31 13:08:49 +0800

    new change

--- MealMaker.js
+++ MealMaker.js
@@ -2,27 +2,42 @@ const menu={
   _courses:{
     appetizers: [],
     mains: [],
-    dessert: []
+    dessert: [],
   },
-  get appetizers (){},
-  set appetizers (app){},
-  get mains (){},
-  set mains (main){},
-  get dessert (){},
-  set dessert (des){},
+  get appetizers() {
+    return this._courses.appetizers;
+  },
+  set appetizers(appetizers) {                        this._courses.appetizers = appetizers;
+  },
+  get mains() {
+    return this._courses.mains;
+  },
+  set mains(mains) {
+   this._courses.mains = mains;
+  },
+  get desserts() {
+    return this._courses.desserts;
+  },
+  set desserts(desserts) {
+   this._courses.desserts = desserts;
+  },
   get _courses (){
-    return  this.appetizers, this.mains, this.dessert
+    return {
+      appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.desserts
+    }
   },
    addDishToCourse (courseName, dishName, dishPrice){
     const dish = {
-      name: courseName,
+      name: dishName,
       price: dishPrice,
     };
-   this._courses[courseName].push(dish);
+   return this._courses[courseName]= dish;
   },
    getRandomDishFromCourse (courseName){
-    const dishes= menu._courses[courseName];
-    let num= Math.floor(Math.random()*dishes.length)
+    const dishes = menu._courses[courseName];
+    const num= Math.floor(Math.random() * dishes.length)
     return dishes[num]
   },
   generateRandomMeal(){
@@ -33,14 +48,15 @@ const menu={
     return `the price is RM${totalPrice} and the items that you ordered are ${appetizer}, ${main} and ${dessert}`
   }
 };
-menu.addDishToCourse('appetizers', 'kacang', "1")
-menu.addDishToCourse('appetizers', 'french fries', "5")
-menu.addDishToCourse('appetizers', 'some french stuff', "7")
-menu.addDishToCourse('main', 'steak', "35")
-menu.addDishToCourse('main', 'chee cheong fun', "10")
-menu.addDishToCourse('main', 'roti canai', "5")
-menu.addDishToCourse('dessert', 'ice cream', "6")
-menu.addDishToCourse('dessert', 'fruits', "15")
-menu.addDishToCourse('dessert', 'chocolate', "9")
+menu.addDishToCourse('appetizers', 'kacang', 1)
+menu.addDishToCourse('appetizers', 'french fries', 5)
+menu.addDishToCourse('appetizers', 'some french stuff', 7)
+menu.addDishToCourse('main', 'steak', 35)
+menu.addDishToCourse('main', 'chee cheong fun', 10)
+menu.addDishToCourse('main', 'roti canai', 5)
+menu.addDishToCourse('dessert', 'ice cream', 6)
+menu.addDishToCourse('dessert', 'fruits', 15)
+menu.addDishToCourse('dessert', 'chocolate', 9)
 
   const meal= menu.generateRandomMeal()
+  console.log(meal)
